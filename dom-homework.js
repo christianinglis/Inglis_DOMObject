43c2efe8b640605108c9b7e79a148b2ac9de9c56
Christian Inglis <ccinglis@my.waketech.edu>
2026-02-25 13:14:53 -0500

    paragraph

--- dom-homework.js
+++ dom-homework.js
@@ -4,3 +4,7 @@ var heading = document.createElement("h1");
 heading.innerHTML = "Welcome to DOM Homework";
 heading.classList.add("highlight");
 myDiv.appendChild(heading);
+
+var paragraph = document.createElement("p");
+paragraph.innerHTML = "This is your first DOM Homework assignment";
+myDiv.appendChild(paragraph);
