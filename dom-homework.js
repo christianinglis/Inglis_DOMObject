a26f6c7514a607b4f9ea57da1e9f684ebc64a6ce
Christian Inglis <ccinglis@my.waketech.edu>
2026-02-25 13:21:05 -0500

    colors

--- dom-homework.js
+++ dom-homework.js
@@ -8,3 +8,30 @@ myDiv.appendChild(heading);
 var paragraph = document.createElement("p");
 paragraph.innerHTML = "This is your first DOM Homework assignment";
 myDiv.appendChild(paragraph);
+
+var ul = document.createElement("ul");
+
+var li1 = document.createElement("li")
+li1.innerHTML = "Item One";
+
+var li2 = document.createElement("li")
+li2.innerHTML = "Item Two";
+
+var li3 = document.createElement("li")
+li3.innerHTML = "Item Three";
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+myDiv.appendChild(ul);
+
+var newItemCounter = 0;
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+}
